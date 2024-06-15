70448ce46152f882d287d563ec016e4a4192bf77
iedwards <iedwards@iedwards.myguest.virtualbox.org>
2024-06-15 18:44:30 +0200

    Updated to live stock ticker.

--- stockfetch.py
+++ stockfetch.py
@@ -1,7 +1,12 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import yfinance as yf
 from consolemenu import *
 from consolemenu.items import *
+from yfinance import shared
+from bs4 import BeautifulSoup
+# import streamlit as st
+import time
 
 #Example code to fetch stock information and plot it:
 
@@ -28,20 +33,39 @@ from consolemenu.items import *
 def search_stock_execute(): #Make more advanced with searching xml file for stock
     stock_name = input("Please enter a stock name to search for:\n")
     #Replace dates with current date -5 years and current date.
-    print(stock_name)
+    
     stock_ticker = yf.Ticker(stock_name)
-    historical_prices = stock_ticker.history(period='1d', interval='1m')
 
+    #Check locally first if exists:
+    with open('US_stock_list.xml', 'r') as f:
+        raw_data = f.read()
+    parsed_data_obj = BeautifulSoup(raw_data, "xml")
+    names = [symbols['name'] for symbols in parsed_data_obj.find_all('symbol')]
+    if (stock_name not in names):
+        if not('longName' in stock_ticker.info.keys()): # Search for some random info that every stock ticker should have, like address1.
+            raise NameError("You did not input a correct stock ticker! Try again please.")
+   
+    plt.ion()
     # Clear the plot and plot the new data
     fig, ax = plt.subplots()
-    ax.clear()
-    ax.plot(historical_prices.index, historical_prices['Close'], label='Stock Value')
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Stock Value')
-    ax.set_title(stock_name + " value")
-    ax.legend(loc='upper left')
-    ax.tick_params(axis='x', rotation=45)
-    fig.show()
+    historical_prices = stock_ticker.history(period='1d', interval='1m')
+    line1, = ax.plot(historical_prices.index, historical_prices['Close'], label='Stock Value')
+    plt.xlabel("Time")
+    plt.ylabel('Stock Value')
+    plt.title(stock_name + " value")
+    plt.legend(loc = 'upper left')
+    plt.tick_params(axis='x', rotation=45)
+
+    while True:
+        #Update data
+        latest_price = historical_prices['Close']
+        latest_time = historical_prices.index
+        line1.set_xdata(latest_time)
+        line1.set_ydata(latest_price)
+        #Plotting time!
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        time.sleep(1)
 
 def display_top_5():
     print("Top 5 here")
@@ -52,11 +76,11 @@ def main():
 
     # A FunctionItem runs a Python function when selected
     search_stock = FunctionItem("Search for a specific stock", search_stock_execute, should_exit=False)
-    top_5 = FunctionItem("Display biggest movers", display_top_5, None)
+    top_5 = FunctionItem("Display biggest movers", display_top_5, should_exit=False)
     menu.append_item(search_stock)
     menu.append_item(top_5)
 
-    # Finally, we call show to show the menu and allow the user to interact
+    # # Finally, we call show to show the menu and allow the user to interact
     menu.show()
         
 if __name__ == '__main__':
